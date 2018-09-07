3a1af734c9b385e97f1e862ecbff640d40143e31
Simon Smulders <simsmulders@gmail.com>
2018-09-07 23:49:28 +0200

    pizzabase

---
+++ pizza-configurator/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import PizzaBase from './components/PizzaBase';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PizzaBase />
+      </div>
+    );
+  }
+}
+
+export default App;
